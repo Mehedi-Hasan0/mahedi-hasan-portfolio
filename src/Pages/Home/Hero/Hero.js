5f9bd560f6c34c12e2449cd6f252cf55451cc319
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2022-12-11 15:14:11 +0600

    skills section added

--- src/Pages/Home/Hero/Hero.js
+++ src/Pages/Home/Hero/Hero.js
@@ -8,7 +8,7 @@ import email from '../../../assets/icon/icons8-envelope-60.png';
 const Hero = () => {
     return (
         <section className='bg-[#121e27]'>
-            <div className=' max-w-6xl mx-auto  py-20 px-5 sm:px-9 md:px-12 lg:px-16 xl:px-0'>
+            <div className=' max-w-6xl mx-auto md:py-32 py-16 px-5 sm:px-9 md:px-12 lg:px-16 xl:px-0'>
                 <div className='flex lg:flex-row flex-col justify-between items-center'>
                     <div className='lg:w-[60%] mb-8 lg:mb-0'>
                         <h2 className='text-white font-medium md:text-5xl sm:text-4xl text-3xl leading-snug'>Hi, I'm Mahedi Hasan <span className='md:text-3xl sm:text-2xl text-xl z-10'>
@@ -22,14 +22,14 @@ const Hero = () => {
                         </span>
                         </h2>
                         <div className='my-5'>
-                            <a href="https://github.com/Mehedi-Hasan0" target='_blank'><img src={github} alt="" className='w-10 inline' /></a>
-                            <a href="https://www.linkedin.com/in/mhmahedihasan" target='_blank'><img src={linkedIn} alt="" className='w-10 inline mx-4' /></a>
+                            <a href="https://github.com/Mehedi-Hasan0" target='_blank' rel="noreferrer"><img src={github} alt="" className='w-10 inline' /></a>
+                            <a href="https://www.linkedin.com/in/mhmahedihasan" target='_blank' rel="noreferrer"><img src={linkedIn} alt="" className='w-10 inline mx-4' /></a>
                             <a href="mailto:dev.mahedihasan@gmail.com"><img src={email} alt="" className='w-10 inline' /></a>
                         </div>
                         <p className='text-white font-light md:w-[60%] md:text-lg text-base opacity-80 mb-8'>I love exploring and creating 🚀 I'm a lifelong learner 🎓 Walking towards my dream of becoming a software engineer 👨‍💻</p>
                         <a
                             href="https://drive.google.com/file/d/1vd2rwoddX8cYxE7wQ4ILg2pqkh2NeQvF/view?usp=sharing"
-                            target='_blank'
+                            target='_blank' rel="noreferrer"
                         >
                             <button className='btn bg-[#d06057] normal-case opacity-90'>See Resume</button>
                         </a>
